4ff29657b45293525464f8cc796fa7955828ef99
Yasanga-1024 <76543968+Yasanga-1024@users.noreply.github.com>
2021-02-06 11:38:43 +0530

    Update sample_config.py

--- tobrot/sample_config.py
+++ tobrot/sample_config.py
@@ -10,7 +10,7 @@ import os
 
 class Config(object):
     # get a token from @BotFather
-    TG_BOT_TOKEN = os.environ.get("TG_BOT_TOKEN", "")
+    TG_BOT_TOKEN = os.environ.get("TG_BOT_TOKEN", )
     # The Telegram API things
     APP_ID = int(os.environ.get("APP_ID", ))
     API_HASH = os.environ.get("API_HASH")
